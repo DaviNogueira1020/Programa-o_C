5f9c000e1cb601934387a3e3c0dbd03b269fd3ee
Draconico-blu <167083051+Draconico-blu@users.noreply.github.com>
2024-11-20 20:14:44 -0300

    Delete jogar.h

--- jogar.h
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-// Função a ser usada externamente
-//botar o a logica do jogo na função ;)
-void jogar() {
-    printf("Função chamada de jogo.c\n");
-    printf("você entrou no jogo\n");
-}
